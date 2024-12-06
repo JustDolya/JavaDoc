b2b5cfe368e3ac5ff954ecd9cf5ee7d94ac222e4
JustDolya <kolya2015d@gmail.com>
2024-12-07 01:07:25 +0300

    Add comments for javadoc

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Book"},{"p":"<Unnamed>","l":"Car"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Person"}];updateSearchResults();
